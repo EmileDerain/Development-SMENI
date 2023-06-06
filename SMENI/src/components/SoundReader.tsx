8478319e0fc06372abc845b6e916734897078c57
TomBevan <bevantom@protonmail.com>
2023-06-06 16:57:57 +0700

    fix: merge

--- SMENI/src/components/SoundReader.tsx
+++ SMENI/src/components/SoundReader.tsx
@@ -1,15 +1,69 @@
 import {View, Text, StyleSheet} from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../assets/colors/colors';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SoundReader = ({transfertInfo}: { transfertInfo: string }) => {
     return (
-        <View>
-            <Text style={[styles.name, styles.text]}>EXEMPLE</Text>
-            <Text style={styles.text2}>{transfertInfo}</Text>
+        <View style={styles.container}>
+            <SafeAreaView style={styles.editWrapper}>
+                <SafeAreaView style={styles.audioEdit}>
+                    <MaterialCommunityIcons
+                        name="play-circle"
+                        size={24}
+                        paddingRight={5}
+                        color={colors.icons}
+                    />
+                    <MaterialCommunityIcons
+                        name="pause-circle"
+                        size={24}
+                        paddingRight={5}
+                        color={colors.icons}
+                    />
+                    <MaterialCommunityIcons
+                        name="volume-minus"
+                        size={24}
+                        paddingRight={5}
+                        color={colors.icons}
+                    />
+                    <MaterialCommunityIcons
+                        name="volume-plus"
+                        size={24}
+                        paddingRight={5}
+                        color={colors.icons}
+                    />
+                </SafeAreaView>
+                <SafeAreaView style={styles.bookmark}>
+                    <MaterialCommunityIcons
+                        name="bookmark-plus"
+                        size={24}
+                        color={colors.icons}
+                    />
+                </SafeAreaView>
+            </SafeAreaView>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    editWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+        marginHorizontal: 30,
+        justifyContent: 'space-between',
+    },
+    audioEdit: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    bookmark: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
     name: {
         fontWeight: "500",
         fontSize: 18,
--- SMENI/src/screens/DiagnoHelpScreen.tsx
+++ SMENI/src/screens/DiagnoHelpScreen.tsx
@@ -1,40 +1,49 @@
 import {useNavigation} from '@react-navigation/native';
-import {
-    Image,
-    Pressable,
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import SoundReader from "../components/SoundReader";
-import { useSelector, useDispatch } from 'react-redux';
-import {exempleSlice} from "../store/exempleSlice";
+import SoundReader from '../components/SoundReader';
+import {useDispatch, useSelector} from 'react-redux';
+import {exempleSlice} from '../store/exempleSlice';
+import colors from '../assets/colors/colors';
 
 const DiagnoHelpScreen = () => {
     const dispatch = useDispatch();
     const navigation = useNavigation();
     //TODO regarde ca: JE DETESTE TS !!!
-    const counterValue = useSelector((state) => state.exemple.value) as number; // Accéder à la valeur de la propriété 'c' dans la tranche 'exemple34'
+    const counterValue = useSelector(state => state.exemple.value) as number; // Accéder à la valeur de la propriété 'c' dans la tranche 'exemple34'
     return (
-        <View style={styles.background}>
-            <Text style={[styles.text, styles.title ]}>Diagnostic Page</Text>
-            <SoundReader transfertInfo={"info"}/>
-            <Pressable
-                onPress={() => {
-                    // update selected product
-                    dispatch(exempleSlice.actions.decrement());
-                }}
-            >
-                <Text style={styles.text}>Counter tg: {counterValue}</Text>
-            </Pressable>
+        <View style={styles.container}>
+            {/* header */}
+            <SafeAreaView style={styles.headerWrapper}>
+                <Text style={[styles.text, styles.title]}>Diagnostic Page</Text>
+                <MaterialCommunityIcons
+                    name="dots-horizontal"
+                    size={24}
+                    color={colors.icons}
+                />
+            </SafeAreaView>
+
+            {/* Audio reader component */}
+            <SoundReader transfertInfo={'info'}/>
+
+            {/* IA prediction*/}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    container: {
+        flex: 1,
         backgroundColor: '#fff',
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 65,
+        marginLeft: 25,
+        marginRight: 20,
     },
     text: {
         color: '#0E1012',
