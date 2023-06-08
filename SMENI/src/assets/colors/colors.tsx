a692efc8d507df99759fb1bd043374455d2b1696
TomBevan <bevantom@protonmail.com>
2023-06-08 17:26:05 +0700

    add: modal

--- SMENI/src/assets/colors/colors.tsx
+++ SMENI/src/assets/colors/colors.tsx
@@ -3,6 +3,7 @@ const colors = {
     background: '#fff',
     icons: '#7B8D9E',
     inputBackground: '#D9D9D9',
+    textLight: '#7F7F7F',
 };
 
 export default colors;
--- SMENI/src/screens/DiagnoHelpScreen.tsx
+++ SMENI/src/screens/DiagnoHelpScreen.tsx
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Pressable, SafeAreaView, StyleSheet, TouchableOpacity, Text, View, TextInput } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  TextInput,
+  Modal,
+  KeyboardAvoidingView,
+  Keyboard,
+} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import SoundReader from '../components/SoundReader';
-import { useSelector } from 'react-redux';
 import colors from '../assets/colors/colors';
 import { useGetShare } from '../useGetShare';
-import React, { useState } from 'react';
 
 const DiagnoHelpScreen = () => {
   const sharedFile = useGetShare();
@@ -20,69 +29,81 @@ const DiagnoHelpScreen = () => {
     'artifact',
     'extrahls',
   ]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleTextChange = (text) => {
     setSelectedValue(text);
+    setShowSuggestions(text.length > 0);
   };
 
   const findSuggestions = (query) => {
     if (query === '') {
       return [];
     }
-  
+
     const regex = new RegExp(`^${query.trim()}`, 'i');
     return suggestions.filter((item) => regex.test(item));
   };
-  
+
   const saveLabeledRecording = () => {
     console.log('saveLabeledRecording');
   };
 
   const filteredSuggestions = findSuggestions(selectedValue);
 
+  const closeSuggestions = () => {
+    setShowSuggestions(false);
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <SafeAreaView style={styles.headerWrapper}>
-        <Text style={[styles.text, styles.title]}>Diagnostic Page</Text>
-        <TouchableOpacity onPress={() => (navigation.canGoBack() ? navigation.goBack() : null)}>
-          <MaterialCommunityIcons name="dots-horizontal" size={24} color={colors.icons} />
-        </TouchableOpacity>
-      </SafeAreaView>
-
-      {/* Audio reader component */}
-      <SoundReader transfertInfo={'info'} />
-
-      {/* IA prediction*/}
-
-      {/* Labelisation */}
-      <SafeAreaView style={styles.labelWrapper}>
-        <Text style={[styles.text, styles.subtitle]}>Label :</Text>
-        <TextInput
-          value={selectedValue}
-          style={styles.input}
-          onChangeText={handleTextChange}
-          placeholder="Select a label..."
-          placeholderTextColor={colors.placeholder}
-        />
-      </SafeAreaView>
-      <SafeAreaView style={styles.suggestionsWrapper}>
-        {filteredSuggestions.map((item) => (
-          <TouchableOpacity
-            key={item}
-            onPress={() => handleTextChange(item)}
-            style={styles.suggestionItem}
-          >
-            <Text style={styles.suggestionText}>{item}</Text>
+      <View style={styles.container}>
+        {/* header */}
+        <SafeAreaView style={styles.headerWrapper}>
+          <Text style={[styles.text, styles.title]}>Diagnostic Page</Text>
+          <TouchableOpacity onPress={() => (navigation.canGoBack() ? navigation.goBack() : null)}>
+            <MaterialCommunityIcons name="dots-horizontal" size={24} color={colors.icons} />
           </TouchableOpacity>
-        ))}
-      </SafeAreaView>
-      <SafeAreaView style={styles.button}>
-        <TouchableOpacity onPress={saveLabeledRecording}>
-          <Text style={[styles.text, styles.title]}>Save</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </View>
+        </SafeAreaView>
+
+        {/* Audio reader component */}
+        <SoundReader transfertInfo={'info'} />
+
+        {/* Labelisation */}
+        <SafeAreaView style={styles.labelWrapper}>
+          <Text style={[styles.text, styles.subtitle]}>Label :</Text>
+          <TextInput
+            value={selectedValue}
+            style={styles.input}
+            onChangeText={handleTextChange}
+            placeholder="Select a label..."
+            placeholderTextColor={colors.textLight}
+          />
+           <Modal visible={showSuggestions} animationType="slide" transparent={true}>
+          <TouchableOpacity style={styles.modalBackground} activeOpacity={1} onPress={closeSuggestions}>
+            <View style={styles.modalContainer}>
+              {filteredSuggestions.map((item) => (
+                <TouchableOpacity
+                  key={item}
+                  onPress={() => handleTextChange(item)}
+                  style={styles.suggestionItem}
+                >
+                  <Text style={styles.suggestionText}>{item}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </TouchableOpacity>
+        </Modal>
+        </SafeAreaView>
+
+       
+
+        <SafeAreaView style={styles.button}>
+          <TouchableOpacity onPress={saveLabeledRecording}>
+            <Text style={[styles.text, styles.title]}>Save</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
   );
 };
 
@@ -101,21 +122,30 @@ const styles = StyleSheet.create({
   },
   labelWrapper: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
     marginTop: 15,
     marginLeft: 30,
+    marginBottom: 20,
   },
-  suggestionsWrapper: {
-    marginTop: 10,
-    marginLeft: 30,
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    paddingVertical: 10,
+    maxHeight: 200,
+    elevation: 4,
   },
   suggestionItem: {
     paddingVertical: 8,
   },
   suggestionText: {
     fontSize: 17,
-    color: colors.text,
+    color: colors.default,
   },
   text: {
     color: '#0E1012',
@@ -135,11 +165,9 @@ const styles = StyleSheet.create({
     fontSize: 17,
   },
   input: {
-    borderColor: colors.default,
-    borderWidth: 1,
     borderRadius: 30,
     backgroundColor: colors.inputBackground,
-    color: colors.text,
+    color: colors.default,
     paddingHorizontal: 15,
     paddingVertical: 10,
     width: '50%',
