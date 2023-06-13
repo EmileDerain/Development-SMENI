f9bab7422463d27ea044fc2a6b079f1675421800
emile <emilederain@gmail.com>
2023-06-13 17:12:15 +0200

    add : prediction

--- backend/controllers/cnn.js
+++ backend/controllers/cnn.js
@@ -1,10 +1,21 @@
 const Audio = require('../models/audio');
 const express = require('express');
-const { exec } = require('child_process');
-const { spawn } = require('child_process');
+const {exec} = require('child_process');
+const {spawn} = require('child_process');
 const serverConf = require("../global/server");
 
 const path = require("path");
+const fs = require("fs");
+
+exports.renameFile = (req, res, next) => {
+    fs.rename(req.file.path, "./CNN/temp/" + req.file.filename, (err) => {
+        if (err) throw err;
+        console.log("File renamed and moved!");
+    });
+    req.file.path = "./CNN/temp/" + req.file.filename;
+    next();
+}
+
 
 exports.getModel = () => {
     express.static(path.join(__dirname, 'CNN/my_model.h5'))
@@ -17,7 +28,7 @@ exports.train = (req, res) => {
 
     var dataToSend;
 
-    res.send({message : "Start building a new model"})
+    res.send({message: "Start building a new model"})
 
     const python = spawn('python', ['CNN/LSTM_Classification.py']);
 
@@ -32,14 +43,30 @@ exports.train = (req, res) => {
         console.log(`child process close all stdio with code ${code}`);
         // send data to browser
     });
+};
 
-    // exec('python CNN/LSTM_Classification.py', (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`Une erreur s'est produite : ${error}`);
-    //         return;
-    //     }
-    //     console.log(`Sortie du script Python : ${stdout}`);
-    // });
-    //
-    // res.status(201).json({message: 'Train en cours'})
+
+exports.predict = async (req, res) => {
+    const python = spawn('python', ['CNN/LSTM_Classification_model.py', req.file.path, './CNN/models/Heart_LSTM_CNN_1.h5']);
+
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+        console.log(dataToSend);
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        if (code === 0) {
+            fs.unlink(req.file.path, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log('Le fichier a été supprimé avec succès: ', req.file.path);
+            });
+        }
+        // send data to browser
+        res.send(JSON.parse(dataToSend));
+    });
 };
--- backend/routes/cnn.js
+++ backend/routes/cnn.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const cnnCtrl = require('../controllers/cnn');
+const multer = require("../middleware/multer-config");
 
 
 router.get('/model', cnnCtrl.getModel);
 router.post('/train', cnnCtrl.train);
 
+router.post('/predict', multer, cnnCtrl.renameFile, cnnCtrl.predict);
 
 module.exports = router;
