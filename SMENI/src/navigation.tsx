e86cdce99ced4b5f207046fda01d7dfa17efc0ef
Vinh <narutovinh@hotmail.fr>
2023-06-13 14:51:35 +0700

    Page signUp

--- SMENI/src/navigation.tsx
+++ SMENI/src/navigation.tsx
@@ -4,6 +4,8 @@ import DiagnoHelpScreen from "./screens/DiagnoHelpScreen";
 import ExempleScreen2 from "./screens/ExempleScreen2";
 import SignUp from "./screens/SignUp";
 import SignIn from "./screens/SignIn";
+import {StyleSheet} from "react-native";
+import colors from "./assets/colors/colors";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,15 +15,19 @@ const Navigation = () => {
             <Stack.Navigator
                 screenOptions={{contentStyle: {backgroundColor: 'blue'}}}
             >
+                <Stack.Screen name="SignUp" component={SignUp} options={{
+                    headerShown: false
+                }}
+                />
                 <Stack.Screen name="DiagnoHelp" component={DiagnoHelpScreen} options={{
                     headerShown: false
                 }}/>
                 <Stack.Screen name="Exemple2" component={ExempleScreen2} />
-                <Stack.Screen name="SignUp" component={SignUp} />
                 <Stack.Screen name="SignIn" component={SignIn} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
+
 
 export default Navigation;
--- SMENI/src/screens/SignUp.tsx
+++ SMENI/src/screens/SignUp.tsx
@@ -1,7 +1,8 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {SafeAreaView, Text, TextInput, View} from "react-native";
+import {SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
+import colors from "../assets/colors/colors";
 
 
 const SignUp = () => {
@@ -12,57 +13,146 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     return (
         // KeyboardAwareScrollView is a ScrollView that automatically adjusts its height when the keyboard appears.
-        <KeyboardAwareScrollView>
+        <KeyboardAwareScrollView style={styles.container}>
             {/*header*/}
-            <SafeAreaView>
-                <View>
-                    <Text>Create Your Account</Text>
-                </View>
+            <SafeAreaView style={styles.headerWrapper}>
+                <Text style={[styles.text, styles.title]}>Create Your Account</Text>
             </SafeAreaView>
             {/*form*/}
             <SafeAreaView>
                 {/*First Name*/}
-                <SafeAreaView>
-                    <Text>First Name</Text>
+                <SafeAreaView style={styles.labelWrapper}>
+                    <Text style={[styles.text, styles.subtitle]}>First Name</Text>
                     <TextInput
                         value={firstName}
+                        style={[styles.input]}
                         placeholder={'First Name'}
                         onChangeText={(text) => setFirstName(text)}
                     />
                 </SafeAreaView>
                 {/*Last Name*/}
-                <SafeAreaView>
-                    <Text>Last Name</Text>
+                <SafeAreaView style={styles.labelWrapper}>
+                    <Text style={[styles.text, styles.subtitle]}>Last Name</Text>
                     <TextInput
                         value={lastName}
+                        style={[styles.input]}
                         placeholder={'Last Name'}
                         onChangeText={(text) => setLastName(text)}
                     />
                 </SafeAreaView>
                 {/*Mail*/}
-                <SafeAreaView>
-                    <Text>Mail</Text>
+                <SafeAreaView style={styles.labelWrapper}>
+                    <Text style={[styles.text, styles.subtitle]}>Mail</Text>
                     <TextInput
                         value={mail}
+                        style={[styles.input]}
                         placeholder={'Mail'}
                         onChangeText={(text) => setMail(text)}
                     />
                 </SafeAreaView>
                 {/*Password*/}
-                <SafeAreaView>
-                    <Text>Password</Text>
+                <SafeAreaView style={styles.labelWrapper}>
+                    <Text style={[styles.text, styles.subtitle]}>Password</Text>
                     <TextInput
                         value={password}
+                        style={[styles.input]}
                         placeholder={'Password'}
                         onChangeText={(text) => setPassword(text)}
                     />
                 </SafeAreaView>
                 {/*TODO : peut être rajouter un champ confirm password    */}
 
+                <SafeAreaView>
+                    <SafeAreaView>
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate('SignIn'); //TODO : créer la méthode pour envoyer le compte au back
+                            }}
+                            disabled={firstName === '' || lastName === '' || mail === '' || password === ''}
+                            style={[styles.buttonContent, styles.button, (firstName === '' || lastName === '' || mail === '' || password === '') && styles.disabledButton]}
+                        >
+                            <Text style={[styles.text, styles.title]}>Create</Text>
+
+                        </TouchableOpacity>
+                    </SafeAreaView>
+
+                </SafeAreaView>
             </SafeAreaView>
 
         </KeyboardAwareScrollView>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 65,
+        marginLeft: 25,
+        marginRight: 20,
+    },
+    labelWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 15,
+        marginLeft: 30,
+        marginBottom: 20,
+    },
+    text: {
+        color: '#0E1012',
+        fontFamily: 'Nunito Sans',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 17,
+        lineHeight: 32,
+        letterSpacing: 0.01,
+    },
+    title: {
+        fontWeight: '700',
+        fontSize: 27,
+    },
+    subtitle: {
+        fontWeight: '700',
+        fontSize: 17,
+    },
+    input: {
+        borderRadius: 30,
+        backgroundColor: colors.inputBackground,
+        borderWidth: 1,
+        borderColor: colors.inputBackground,
+        color: colors.default,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        width: '50%',
+        marginLeft: 10,
+    },
+    invalidInput: {
+        borderColor: 'red', // Couleur de la bordure en cas d'input invalide
+    },
+    button: {
+        borderColor: colors.default,
+        borderWidth: 1,
+        borderRadius: 30,
+        alignContent: 'center',
+        alignItems: 'center',
+        width: '40%',
+        padding: 10,
+        alignSelf: 'center',
+        marginVertical: 20,
+    },
+    buttonContent: {
+        flex: 1,
+        opacity: 1, // Default opacity when the button is enabled
+    },
+    disabledButton: {
+        opacity: 0.5, // Opacity when the button is disabled
+    },
+});
 
 export default SignUp;
