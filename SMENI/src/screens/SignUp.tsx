8a72bf2f2ef3e21e80582cd7daf9653bc3361493
Vinh <narutovinh@hotmail.fr>
2023-06-13 10:51:41 +0700

    Suite

--- SMENI/src/screens/SignUp.tsx
+++ SMENI/src/screens/SignUp.tsx
@@ -10,7 +10,6 @@ const SignUp = () => {
     const [lastName, setLastName] = useState('');
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
-    const [phone, setPhone] = useState('');
     return (
         // KeyboardAwareScrollView is a ScrollView that automatically adjusts its height when the keyboard appears.
         <KeyboardAwareScrollView>
@@ -22,15 +21,43 @@ const SignUp = () => {
             </SafeAreaView>
             {/*form*/}
             <SafeAreaView>
+                {/*First Name*/}
                 <SafeAreaView>
                     <Text>First Name</Text>
                     <TextInput
-                    value={firstName}
-                    placeholder={'First Name'}
-                    onChangeText={(text) => setFirstName(text)}
+                        value={firstName}
+                        placeholder={'First Name'}
+                        onChangeText={(text) => setFirstName(text)}
                     />
-
                 </SafeAreaView>
+                {/*Last Name*/}
+                <SafeAreaView>
+                    <Text>Last Name</Text>
+                    <TextInput
+                        value={lastName}
+                        placeholder={'Last Name'}
+                        onChangeText={(text) => setLastName(text)}
+                    />
+                </SafeAreaView>
+                {/*Mail*/}
+                <SafeAreaView>
+                    <Text>Mail</Text>
+                    <TextInput
+                        value={mail}
+                        placeholder={'Mail'}
+                        onChangeText={(text) => setMail(text)}
+                    />
+                </SafeAreaView>
+                {/*Password*/}
+                <SafeAreaView>
+                    <Text>Password</Text>
+                    <TextInput
+                        value={password}
+                        placeholder={'Password'}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                </SafeAreaView>
+                {/*TODO : peut être rajouter un champ confirm password    */}
 
             </SafeAreaView>
 
