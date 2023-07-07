4504125e19c5b55f265c3bf8605f5810c94c4943
Vinh <narutovinh@hotmail.fr>
2023-07-07 09:15:56 +0700

    Test

--- SMENI/src/screens/NavigationBar.tsx
+++ SMENI/src/screens/NavigationBar.tsx
@@ -1,54 +1,57 @@
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet} from "react-native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {PAGE_DIAGNOHELP, PAGE_HOME, PAGE_SEARCH_PATIENT} from "../utils/path";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SearchPatient from "./SearchPatient";
 import DiagnoHelpScreen from "./DiagnoHelpScreen";
 import Home from "./Home";
+import SignIn from "./SignIn";
 
 
 const Tab = createBottomTabNavigator();
 
 const NavigationBar = () => {
-    const navigation = useNavigation();
 
+    return (
+        <Tab.Navigator
+            initialRouteName={PAGE_HOME}
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    let rn = route.name;
 
-    // @ts-ignore
-    return (// KeyboardAwareScrollView is a ScrollView that automatically adjusts its height when the keyboard appears.
-            <Tab.Navigator
-                initialRouteName={PAGE_HOME}
-                screenOptions={({route}) => ({
-                    tabBarIcon: ({focused, color, size}) => {
-                        let iconName;
-                        let rn = route.name;
+                    if (rn === PAGE_HOME) {
+                        iconName = focused ? 'home' : 'home-outline';
+                    } else if (rn === PAGE_SEARCH_PATIENT) {
+                        iconName = focused ? 'search' : 'search-outline';
+                    } else if (rn === PAGE_DIAGNOHELP) {
+                        iconName = focused ? 'help-circle' : 'help-circle-outline';
+                    }
 
-                        if (rn === PAGE_HOME) {
-                            iconName = focused ? 'home' : 'home-outline';
-                        } else if (rn === PAGE_SEARCH_PATIENT) {
-                            iconName = focused ? 'search' : 'search-outline';
-                        } else if (rn === PAGE_DIAGNOHELP) {
-                            iconName = focused ? 'help-circle' : 'help-circle-outline';
-                        }
+                    return <Ionicons name={iconName} size={size} color={color}/>;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'blue',
+                inactiveTintColor: 'gray',
+                labelStyle: {paddingBottom: 10, fontSize: 1},
+                style: {height: 70, padding: 10}
+            }
+            }
+        >
+            <Tab.Screen name={PAGE_HOME} component={Home} options={{
+                headerShown: false
+            }}/>
+            <Tab.Screen name={PAGE_SEARCH_PATIENT} component={SearchPatient} options={{
+                headerShown: false
+            }}/>
+            <Tab.Screen name={PAGE_DIAGNOHELP} component={SignIn} options={{
+                headerShown: false
+            }}/>
 
-                        return <Ionicons name={iconName} size={size} color={color}/>;
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'blue',
-                    inactiveTintColor: 'gray',
-                    labelStyle: {paddingBottom: 10, fontSize: 1},
-                    style: {height: 70, padding: 10}
-                }
-                }
-            >
-                <Tab.Screen name={PAGE_HOME} component={Home}/>
-                <Tab.Screen name={PAGE_SEARCH_PATIENT} component={SearchPatient}/>
-                <Tab.Screen name={PAGE_DIAGNOHELP} component={DiagnoHelpScreen}/>
-
-            </Tab.Navigator>
-       )
+        </Tab.Navigator>
+    )
 }
 
 export default NavigationBar;
--- SMENI/src/screens/SearchPatient.tsx
+++ SMENI/src/screens/SearchPatient.tsx
@@ -1,30 +1,19 @@
-import {useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from "react-native";
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {PAGE_SIGNIN} from "../utils/path";
-import {isTokenValid} from "../utils/jwtCheck";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import NavigationBar from "./NavigationBar";
+import {PAGE_DIAGNOHELP, PAGE_HOME, PAGE_SEARCH_PATIENT} from "../utils/path";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Home from "./Home";
+import SignIn from "./SignIn";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
-
-const CheckToken = async () => {
-    const navigation = useNavigation();
-    const tokenFromStorage = await AsyncStorage.getItem('token');
-    if (!isTokenValid(tokenFromStorage)) {
-        console.log("token is not valid")
-        navigation.navigate(PAGE_SIGNIN);
-    }
-}
 
 const Tab = createBottomTabNavigator();
 
+
 const SearchPatient = () => {
-    const navigation = useNavigation();
-
-    //CheckToken();
-
 
     return (// KeyboardAwareScrollView is a ScrollView that automatically adjusts its height when the keyboard appears.
         <KeyboardAwareScrollView style={styles.container}>
@@ -33,8 +22,41 @@ const SearchPatient = () => {
                 <Text>Content goes there compris ?</Text>
             </SafeAreaView>
             {/*footer*/}
+            <Tab.Navigator
+                initialRouteName={PAGE_HOME}
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                        let iconName;
+                        let rn = route.name;
 
-            <NavigationBar/>
+                        if (rn === PAGE_HOME) {
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (rn === PAGE_SEARCH_PATIENT) {
+                            iconName = focused ? 'search' : 'search-outline';
+                        } else if (rn === PAGE_DIAGNOHELP) {
+                            iconName = focused ? 'help-circle' : 'help-circle-outline';
+                        }
+
+                        return <Ionicons name={iconName} size={size} color={color}/>;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'blue',
+                    inactiveTintColor: 'gray',
+                    labelStyle: {paddingBottom: 10, fontSize: 1},
+                    style: {height: 70, padding: 10}
+                }
+                }
+            >
+                <Tab.Screen name={PAGE_SEARCH_PATIENT} component={SearchPatient} options={{
+                    headerShown: false
+                }}/>
+                <Tab.Screen name={PAGE_DIAGNOHELP} component={SignIn} options={{
+                    headerShown: false
+                }}/>
+
+            </Tab.Navigator>
+
         </KeyboardAwareScrollView>
     )
 
@@ -43,11 +65,11 @@ const SearchPatient = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex:1, backgroundColor: '#fff',
+        flex: 1, backgroundColor: '#fff',
     }, content: {
         flex: 1, justifyContent: 'center', alignItems: 'center',
     }, footer: {
-        backgroundColor: "blue", bottom:0
+        backgroundColor: "blue",
     }
 });
 export default SearchPatient;
