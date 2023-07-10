edb7f6cacf6217befa292f7bf2b5f07358ee6310
Vinh <narutovinh@hotmail.fr>
2023-07-10 10:12:09 +0700

    Ajout du controller

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -1,4 +1,25 @@
 const Patient = require('../models/patient');
+
+exports.createPatient = (req, res) => {
+    const patient = new Patient({
+        firstName: req.body.firstName, lastName: req.body.lastName, height: req.body.height, weight: req.body.weight, birthDate: req.body.birthDate
+    });
+    patient.save()
+        .then(() => res.status(201).json({message: 'Patient created !'}))
+        .catch(error => res.status(400).json({error, message: 'Error while creating patient !'}));
+};
+
+exports.getAllPatient = (req, res) => {
+    Patient.find()
+        .then(patients => res.status(200).json({"patients": patients, message: 'All the patients have been retrieved'}))
+        .catch(error => res.status(400).json({error, message: 'Error while retrieving all the patients'}));
+}
+
+exports.getPatientByName = (req, res) => {
+    Patient.find({ $or: [{ firstName: req.body.name }, { lastName: req.body.name }] })
+        .then(patient => res.status(200).json({patient, message: 'Patient retrieved'}))
+        .catch(error => res.status(400).json({error, message: 'Error while retrieving patient'}));
+}
 const Label = require("../models/label");
 
 
